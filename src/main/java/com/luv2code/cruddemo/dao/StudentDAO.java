e1540b3ff476ae33bfe70d0f456d7d7ee8fd7a09
Vanessa Mendoza <v.mendoza1@gmail.com>
2023-10-14 18:32:43 -0400

    Update and Delete objects

--- src/main/java/com/luv2code/cruddemo/dao/StudentDAO.java
+++ src/main/java/com/luv2code/cruddemo/dao/StudentDAO.java
@@ -10,4 +10,14 @@ public interface StudentDAO {
     Student findById(Integer id);
 
     List<Student> findAll();
+
+    List<Student>findByLastName(String theLastName);
+
+    void update(Student theStudent);
+
+    void delete (Integer id);
+
+    int deleteAll();
+
+
 }
--- src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
+++ src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
@@ -37,5 +37,45 @@ public class StudentDAOImpl implements StudentDAO{
         return theQuery.getResultList();
     }
 
+    @Override
+    public List<Student> findByLastName(String theLastName) {
+        // create query
+        TypedQuery<Student> theQuery = entityManager.createQuery(
+                "FROM Student WHERE lastName=:theData", Student.class);
+
+        // set query parameter
+        theQuery.setParameter("theData", theLastName);
+
+        // return query results
+        return theQuery.getResultList();
+    }
+
+    @Override
+    @Transactional
+    public void update(Student theStudent) {
+        entityManager.merge(theStudent);
+
+    }
+
+    @Override
+    @Transactional
+    public void delete(Integer id) {
+
+        // retrieve the student
+        Student theStudent = entityManager.find(Student.class, id);
+
+        // delete the student
+        entityManager.remove(theStudent);
+    }
+
+    @Override
+    @Transactional
+    public int deleteAll() {
+
+        int numRowsDeleted = entityManager.createQuery("DELETE FROM Student").executeUpdate();
+
+        return numRowsDeleted;
+    }
+
 
 }
